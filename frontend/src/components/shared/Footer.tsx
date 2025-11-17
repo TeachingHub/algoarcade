bc32b3cdd6c47d24742ecf68ae09bee10379699d
danisntoss <danisant1504.dsl@gmail.com>
2025-11-17 19:53:33 +0100

    Style: Refactor Footer component layout and update global text styles; enhance readability and structure

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,5 +1,4 @@
 import styles from "@/styles/components/shared/Footer.module.css";
-import Button from "./Button";
 import Divider from "./Divider";
 
 
@@ -11,20 +10,32 @@ export default function Footer() {
                     <div className={styles.section}>
                         <h3>About Us</h3>
                         <p>Information about the company.</p>
+                        <p>Information about the company.</p>
+                        <p>Information about the company.</p>
+                        <p>Information about the company.</p>
+                        <p>Information about the company.</p>
+                        <p>Information about the company.</p>
+                        <p>Information about the company.</p>
                     </div>
                     <div className={styles.section}>
                         <h3>Contact</h3>
                         <p>Email: contact@algoarcade.com</p>
+                        <p>Email: contact@algoarcade.com</p>
+                        <p>Email: contact@algoarcade.com</p>
+                        <p>Email: contact@algoarcade.com</p>
                     </div>
                     <div className={styles.section}>
-                        <h3>Follow Us</h3>
-                        <div className={styles.socialButtons}>
-                            <Button style={["link"]} label="Twitter" to="https://twitter.com/algoarcade" target="blank" />
-                            <Button style={["link"]} label="Facebook" to="https://facebook.com/algoarcade" target="blank" />
-                            <Button style={["link"]} label="Instagram" to="https://instagram.com/algoarcade" target="blank" />
-                        </div>
+                        <h3>Juegos</h3>
+                        <p> {'>'}juego 1: TSP</p>
+                        <p> {'>'}juego 2: Pathfinder A*</p>
+                        <p> {'>'}juego 3: visualgo</p>
+                        <p> {'>'}juego 1: TSP</p>
+                        <p> {'>'}juego 2: Pathfinder A*</p>
+                        <p> {'>'}juego 3: visualgo</p>
+                        <p> {'>'}juego 1: TSP</p>
+                        <p> {'>'}juego 2: Pathfinder A*</p>
+                        <p> {'>'}juego 3: visualgo</p>
                     </div>
-                
                 </div>
                 <Divider size="large" thickness="medium" />
                 <div className={styles.bottom}>
